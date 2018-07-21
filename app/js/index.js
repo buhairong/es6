6de6948c6e338e9077cb536b33b4097cddd2655a
buhairong <22858111@qq.com>
2018-07-21 13:25:19 +0800

    add file

--- app/js/index.js
+++ app/js/index.js
@@ -1,11 +1,3 @@
-//import 'babel-polyfill';
-//import './class/lesson17';
+import 'babel-polyfill';
+import lottery from './lottery';
 
-//import {A,test,Hello} from './class/lesson17';
-//import {A} from './class/lesson17';
-//import * as lesson from './class/lesson17';
-import lesson17 from './class/lesson17';
-
-//console.log(A,test,Hello);
-//console.log(lesson.A,lesson.test,lesson.Hello);
-console.log(lesson17.A);
